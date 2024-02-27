aa80b42c54de1e07d992e20803b4d0d10f2ba576
Manu Cornet <m@ma.nu>
2024-02-27 17:34:10 +0100

    Automatically load profile. Some Tweets (re-posts) can't be deleted.

--- background.js
+++ background.js
@@ -9,6 +9,15 @@ chrome.action.onClicked.addListener((tab) => {
     args: [],
     func: async () => {
 
+      const loadProfile = () => {
+        const profileButton = document.querySelector('[data-testid="AppTabBar_Profile_Link"]');
+        profileButton.click();
+      };
+
+      loadProfile();
+      // Sleep
+      await new Promise(r => setTimeout(r, 2500));
+
       const scrollToBottom = async () => {
         let scrollableHeight =
             document.documentElement.scrollHeight - window.innerHeight
@@ -25,10 +34,11 @@ chrome.action.onClicked.addListener((tab) => {
 
       await scrollToBottom();
 
+      let cantDeleteTweetCount = 0;
+
       const deleteOldestTweet = async () => {
         const tweets = document.querySelectorAll('[data-testid="tweet"]');
-        const moreMenu = tweets[tweets.length - 1].querySelector('[aria-label="More"]');
-        // const moreMenus = document.querySelectorAll('[aria-label="More"]');
+        let moreMenu = tweets[tweets.length - 1 - cantDeleteTweetCount].querySelector('[data-testid="caret"]');
         if (!tweets.length) {
           alert('I did not find any Tweets here. Please load your profile page first.');
           return;
@@ -38,23 +48,27 @@ chrome.action.onClicked.addListener((tab) => {
           moreMenu.click();
           const menuItems = document.querySelectorAll('[role="menuitem"]');
           for (const item of menuItems) {
-            if (item.innerText === 'Delete') {
+            if (item.innerText === 'Delete' || item.innerText === 'Supprimer') {
               item.click();
               break;
             }
           }
           const buttons = document.querySelectorAll('[role="button"]');
           for (const button of buttons) {
-            if (button.innerText === 'Delete') {
+            if (button.innerText === 'Delete' || button.innerText === 'Supprimer') {
               button.click();
               break;
             }
+            // Assume we can't delete this.
+            cantDeleteTweetCount++;
           }
+        } else {
+          console.log('Could not find a more menu');
         }
       };
 
       let remainingTweets = document.querySelectorAll('[data-testid="tweet"]');
-      while (remainingTweets.length) {
+      while (remainingTweets.length > cantDeleteTweetCount) {
         deleteOldestTweet();
         // Sleep
         await new Promise(r => setTimeout(r, 2500));
