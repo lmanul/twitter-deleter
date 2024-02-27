9156f7fd24931ae9fe3b6d46b2f816a7bfa2831e
Manu Cornet <m@ma.nu>
2024-02-27 18:24:25 +0100

    Try to be language-agnostic

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.action.onClicked.addListener((tab) => {
     args: [],
     func: async () => {
 
-      const scrollMargin = 500;
+      const scrollMargin = 600;
       const profileButton = document.querySelector('[data-testid="AppTabBar_Profile_Link"]');
       profileButton.click();
 
@@ -36,48 +36,48 @@ chrome.action.onClicked.addListener((tab) => {
       const deleteOldestTweet = async () => {
         const tweets = document.querySelectorAll('[data-testid="tweet"]');
         const oldestTweet = tweets[tweets.length - 1];
-        let moreMenu = oldestTweet.querySelector('[data-testid="caret"]');
         if (!tweets.length) {
-          alert('I did not find any Tweets here. Please load your profile page first.');
+          alert('I did not find any Tweets here.');
           return;
         }
 
+        // Let's see if this is a retweet.
+        const unretweetButton = oldestTweet.querySelector('[data-testid="unretweet"]');
+        if (unretweetButton) {
+          retweetButton.click();
+          await new Promise(r => setTimeout(r, 1000));  // Sleep
+          const confirm = document.querySelector('[data-testid="unretweetConfirm"]');
+          confirm.click();
+          await new Promise(r => setTimeout(r, 1000));  // Sleep
+          return;  // Done with this Tweet
+        }
+
+        let moreMenu = oldestTweet.querySelector('[data-testid="caret"]');
         if (moreMenu) {
           moreMenu.click();
           let foundDelete = false;
           const menuItems = document.querySelectorAll('[role="menuitem"]');
-          for (const item of menuItems) {
-            if (item.innerText === 'Delete' || item.innerText === 'Supprimer') {
-              foundDelete = true;
-              item.click();
-              break;
-            }
+          if (!menuItems.length) {
+            return;  // Give up on this Tweet
           }
-          if (!foundDelete) {
-            // This is probably a reTweet.
-            // Dismiss the dialog
-            document.body.dispatchEvent(new KeyboardEvent('keypress'), {'key': 'Escape'});
-            const retweetButton = oldestTweet.querySelector('[data-testid="unretweet"]');
-            retweetButton.click();
-            await new Promise(r => setTimeout(r, 1000));  // Sleep
-            const confirm = document.querySelector('[data-testid="unretweetConfirm"]');
-            confirm.click();
-            await new Promise(r => setTimeout(r, 1000));  // Sleep
-          } else {
-            const buttons = document.querySelectorAll('[role="button"]');
-            for (const button of buttons) {
-              if (button.innerText === 'Delete' || button.innerText === 'Supprimer') {
-                button.click();
-                break;
-              }
-            }
+          // To avoid having to handle translations, assume the item we want
+          // is the first one.
+          menuItems[0].click();
+          await new Promise(r => setTimeout(r, 1000));  // Sleep
+          const buttons = document.querySelectorAll('[role="button"]');
+          if (!buttons.length) {
+            return;  // Give up on this Tweet
           }
+          // The Delete button is the first one.
+          buttons[0].click();
+          await new Promise(r => setTimeout(r, 1000));  // Sleep
         } else {
           console.log('Could not find a more menu');
         }
       };
 
       let remainingTweets = document.querySelectorAll('[data-testid="tweet"]');
+      console.log('I see ' + remainingTweets.length + ' Tweets remaining.');
       while (remainingTweets.length > 0) {
         deleteOldestTweet();
         await scrollToBottom();
